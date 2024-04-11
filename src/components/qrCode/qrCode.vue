41ed0becec4c5a7e9cfd4aab8d65e4c45faae8cb
Manuel Alejandro <manuel.alejandro@oliveiratrust.com.br>
2024-04-10 22:40:13 -0300

    Atualizar componente qrCode.vue e ContactUs.vue

---
+++ src/components/qrCode/qrCode.vue
@@ -0,0 +1,66 @@
+<template>
+<div id="qr-code">
+    <p id="cabreras-section4">CABRERAS</p>
+    <p id="unindo-mercados-section4">UNINDO MERCADOS</p>
+    <img src="../../../public/img/vcard_qrcode.png" alt="QRcode v-card" id="QRcode-v-card-section-4">
+</div>
+</template>
+
+<script>
+export default {
+    name: 'QrCode',
+    data() {
+        return {
+            nav:true,
+            mobileView:false,
+        }
+    }
+
+}
+</script>
+
+<style>
+#cabreras-section4{
+color: var(--colorSec-BeigeLig, #F0C986);
+leading-trim: both;
+text-edge: cap;
+/* Shadow/BlueDark/50% */
+text-shadow: 0rem 0.25rem 0.25rem rgba(6, 35, 49, 0.50);
+font-family: "Lexend Exa";
+font-size: 3rem;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+letter-spacing: 0.36rem;
+}
+
+#unindo-mercados-section4{
+color: var(--colorSec-BeigeLig, #F0C986);
+leading-trim: both;
+text-edge: cap;
+/* Shadow/BlueDark/50% */
+text-shadow: 0rem 0.25rem 0.25rem rgba(6, 35, 49, 0.50);
+font-family: "Lexend Exa";
+font-size: 1.25rem;
+font-style: normal;
+font-weight: 300;
+line-height: normal;
+letter-spacing: 0.4125rem;
+}
+#QRcode-v-card-section-4{
+    width: 13.125rem;
+    height: 13.125rem;
+    flex-shrink: 0;
+    margin-top: 0.9375rem;
+    }
+
+#qr-code{
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 9.4375rem;
+}
+</style>
+
+
+    
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -2,18 +2,14 @@
     <section class="contate-nos-section4">
 
     <div class="contate-nos">
-        <div id="contato-firstbox">
+        <div>
             <div id="info-required">
                 <p id="contact-us">Contate-nos</p>
                 <cabrera-logo-text-vue />
                 <contact-form/>
             </div>
             </div>
-            <div id="qr-code">
-                <p id="cabreras-section4">CABRERAS</p>
-                <p id="unindo-mercados-section4">UNINDO MERCADOS</p>
-                <img src="../../public/img/vcard_qrcode.png" alt="QRcode v-card" id="QRcode-v-card-section-4">
-        </div>
+            <qr-code/>
         </div>  
         <privacy-politics />
     </section>
@@ -25,13 +21,15 @@
 import cabreraLogoTextVue from '../components/cabrerasLogoText/cabreraLogoText.vue'
 import contactForm from '../components/forms/contactForm.vue'
 import privacyPolitics from '../components/privacyPolitics/privacyPolitics.vue'
+import qrCode from '../components/qrCode/qrCode.vue'
 
 export default {
     name: 'ContactUs',
     components: {
         cabreraLogoTextVue,
         contactForm,
-        privacyPolitics
+        privacyPolitics,
+        qrCode
     },
     data() {
         return {
@@ -44,7 +42,6 @@ export default {
 
 
 <style scoped>
-
 
 
 .contate-nos-section4{
@@ -72,88 +69,5 @@ line-height: normal;
 letter-spacing: 0.075rem;
 margin-bottom: 1.25rem;
 }
-
-#logo-text{
-display: flex;
-  
-}
-
-
-#send-message{
-width: 13.9375rem;
-height: 2.8125rem;
-flex-shrink: 0;
-border-radius: 0.25rem;
-background: var(--clr-Beige-Light, #F0C986);
-color: var(--clr-Blue-Light, #113649);
-font-family: "Lexend Deca";
-font-size: 0.875rem;
-font-style: normal;
-font-weight: 500;
-line-height: 150%; /* 21px */
-letter-spacing: 0.0262rem;
-margin-top: 0.9375rem;
-}
-#cabreras-section4{
-color: var(--colorSec-BeigeLig, #F0C986);
-leading-trim: both;
-text-edge: cap;
-/* Shadow/BlueDark/50% */
-text-shadow: 0rem 0.25rem 0.25rem rgba(6, 35, 49, 0.50);
-font-family: "Lexend Exa";
-font-size: 3rem;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-letter-spacing: 0.36rem;
-}
-
-#unindo-mercados-section4{
-color: var(--colorSec-BeigeLig, #F0C986);
-leading-trim: both;
-text-edge: cap;
-/* Shadow/BlueDark/50% */
-text-shadow: 0rem 0.25rem 0.25rem rgba(6, 35, 49, 0.50);
-font-family: "Lexend Exa";
-font-size: 1.25rem;
-font-style: normal;
-font-weight: 300;
-line-height: normal;
-letter-spacing: 0.4125rem;
-}
-
-#QRcode-v-card-section-4{
-width: 13.125rem;
-height: 13.125rem;
-flex-shrink: 0;
-margin-top: 0.9375rem;
-}
-
-#inputs-button{
-display: flex;
-flex-direction: column;  
-  
-
-}
-
-#form{
-display: flex;
-flex-direction: column;  
-margin-top: 0.9375rem;
-
-}
-
-#qr-code{
-display: flex;
-flex-direction: column;
-align-items: center;
-margin-top: 9.4375rem;
-}
-
-#info-required{
-margin-top: 3rem;
-}
-
-
 
 </style>
