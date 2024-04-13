7a88a1921b1ab15f5c7bd97b058f5f9ace75d181
Alian Medina <alianmc1988@gmail.com>
2024-04-12 22:05:54 -0300

    container who we are

---
+++ src/components/whoWeAre/whoWeAre.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="whowearebox">
+		<img :url_image="url_image" />
+		<div class="text-box1">
+			<div id="paragraphs_box1">
+				<br />
+				<p id="second-text-box1">
+					CABRERAS é uma empresa de comércio eletrônico que conecta exportadores
+					brasileiros com importadores internacionais.
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import img from '../../public/img/QSoomos_1.webp'
+export default {
+	name: 'WhoWeAre',
+	components: {
+		img,
+	},
+	props: {
+		url_image: {
+			type: String,
+			default: 'No_image',
+		},
+		description: {
+			type: String,
+			default: 'No_description',
+		},
+
+		id: {
+			type: String,
+			default: 'No_id',
+		},
+	},
+}
+</script>
---
+++ src/data/imagesData.js
@@ -0,0 +1,20 @@
+export const imagesData = [
+	{
+		name: 'QSoomos_1',
+		url_image: '../../public/img/QSoomos_1.webp',
+		paragraph:
+			'CABRERAS é uma empresa de comércio eletrônico que conecta exportadores brasileiros com importadores internacionais.',
+	},
+	{
+		name: 'QSoomos_2',
+		url_image: '../../public/img/QSoomos_2.jpg',
+		paragraph:
+			'Atuamos como intermediário eficiente e confiável, facilitando a conexão entre exportadores brasileiros e clientes estrangeiros.',
+	},
+	{
+		name: 'QSoomos_3',
+		url_image: '../../public/img/QSoomos_3.jpg',
+		paragraph:
+			'Nosso modelo de negócio se baseia em criar conexões que proporcionem benefícios significativos para todas as partes envolvidas.',
+	},
+]
